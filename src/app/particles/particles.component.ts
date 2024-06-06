04e65546e04f0605c308d19f2438d78dc7c39f07
David <dwebster182@gmail.com>
2024-06-06 14:13:21 +0200

    simple + dave2.0

--- src/app/particles/particles.component.ts
+++ src/app/particles/particles.component.ts
@@ -4,9 +4,6 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
-// Make sure to correctly import OBJLoader based on your setup
-
 @Component({
   selector: 'app-particles',
   templateUrl: './particles.component.html',
@@ -16,17 +13,14 @@ export class ParticlesComponent implements AfterViewInit {
 
   @ViewChild('rendererContainer') rendererContainer!: ElementRef;
 
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({ alpha: true });
   scene;
   camera;
-  width = window.innerWidth;
-  height = window.innerHeight;
   controls: any;
   frame = 0;
   active: boolean = true;
-  question = ""
+  question = "";
   isDisabled: boolean = false;
-
 
   public myMessage = 'Hello, this is Dave 2.0 - david@webstarcloud.com';
   public displayedMessage = '';
@@ -38,19 +32,10 @@ export class ParticlesComponent implements AfterViewInit {
 
   constructor(private http: HttpClient) {
     this.scene = new THREE.Scene();
-    // this.scene.background = new THREE.Color(0x6f2da8);
-    this.scene.background = new THREE.Color(0xffffff);
+    this.scene.background = null; // Ensure background is transparent
 
-    this.camera = new THREE.PerspectiveCamera(55, 1, 0.1, 2000);
-
-    // this.camera.position.set(0, 90, 10);  // Move the camera back and align it with the object's position
-    // this.camera.lookAt(0, 90, 0);  // Make the camera look at the object
-    // this.camera.aspect = this.width / this.height;
-    // this.camera.updateProjectionMatrix();
-
+    this.camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 2000);
     this.camera.position.set(0, 0, -1);
-    this.camera.aspect = this.width / this.height;
-    this.camera.updateProjectionMatrix();
 
     // Lighting
     const ambientLight = new THREE.AmbientLight(0xffffff, 1);
@@ -61,25 +46,16 @@ export class ParticlesComponent implements AfterViewInit {
     this.scene.add(pointLight);
 
     this.loadOBJModel();
-
     this.startTyping(this.myMessage);
-
   }
 
   getData(prompt: string) {
-
     let api_key = "rSxnSS5RnZ4HqW1lxzY1T8py4F0hYoLH9sVFTqHI"
 
-    const body = {
-      "prompt": prompt
-    };
-
+    const body = { "prompt": prompt };
     const headers = new HttpHeaders({
       "Content-Type": "application/json",
       "x-api-key": api_key
-      // "Authorization": access_token,
-      // "Access-Control-Allow-Origin": "*", 
-      // "Access-Control-Allow-Credentials": "true"
     });
 
     this.http.post('https://clzngwfhz1.execute-api.eu-west-1.amazonaws.com/test', body, { headers }).subscribe(response => {
@@ -112,11 +88,9 @@ export class ParticlesComponent implements AfterViewInit {
   }
 
   stopDotsAnimation() {
-    console.log("Stopping dots animation"); // Debugging line
     clearInterval(this.dotsIntervalId);
     this.displayedDots = ''; // Reset the dots string
   }
-
 
   startTyping(my_msg: string) {
     let i = 0;
@@ -132,85 +106,58 @@ export class ParticlesComponent implements AfterViewInit {
     }, this.speed);
   }
 
-
   resetObject() {
     // Traverse the scene and find the object by name
     this.scene.traverse((object: any) => {
       if (object.name === 'myObject') {
-        console.log('Object found:', object);
-
         // Reset the object's position, rotation, and scale to the initial values
         object.position.set(-30, 90, 0);
         object.rotation.set(0, 0, 0);
         object.rotation.z = THREE.MathUtils.degToRad(90);
         object.rotation.y = THREE.MathUtils.degToRad(-10);
         object.scale.set(10, 10, 10);
-
-        console.log('Object reset:', object);
       }
     });
   }
-
 
   loadOBJModel = () => {
     const loader = new OBJLoader();
     loader.load(
       '../../assets/me.obj',
       (object: any) => {
-
-        // object.rotation.set(0, 0, 0);
         object.position.set(80, 20, -20);
         object.name = 'myObject';
         object.scale.set(10, 10, 10); // Adjust as necessary
 
-        object.rotation.z = THREE.MathUtils.degToRad(90)
-        object.rotation.y = THREE.MathUtils.degToRad(-10)
-        object.rotation.x = THREE.MathUtils.degToRad(-10)
+        object.rotation.z = THREE.MathUtils.degToRad(90);
+        object.rotation.y = THREE.MathUtils.degToRad(-10);
+        object.rotation.x = THREE.MathUtils.degToRad(-10);
 
         const vertexShader = `
-  varying vec2 vUv;
-  uniform float time;
-  void main() {
-    vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }
-`;
-
+          uniform float pointSize;
+          varying vec2 vUv;
+          uniform float time;
+          void main() {
+            gl_PointSize = pointSize;
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+          }
+        `;
 
         const fragmentShader = `
-  varying vec2 vUv;
-  void main() {
-    gl_FragColor = vec4(vUv, 0.5, 1.0);
-  }
-`;
+          void main() {
+            gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0); // RGBA for purple
+          }
+        `;
 
         const material = new THREE.ShaderMaterial({
           vertexShader: vertexShader,
           fragmentShader: fragmentShader,
           uniforms: {
+            pointSize: { value: 1 },
             time: { value: 0 }
           }
         });
-
-        //   object.traverse((child: any) => {
-        //     if (child instanceof THREE.Mesh) {
-        //         child.material.color.set(0x00FF00);
-        //         const positionAttribute = child.geometry.getAttribute('position');
-        //         const positionArray = positionAttribute.array as Float32Array;
-
-        //         for (let i = 0; i < positionArray.length; i += 3) {
-        //             const x = positionArray[i];
-        //             const y = positionArray[i + 1];
-        //             const z = positionArray[i + 2];
-
-        //             positionArray[i] = x + Math.cos(this.frame + x) * 0.1;
-        //             positionArray[i + 1] = y + Math.sin(this.frame + y) * 0.1;
-        //             positionArray[i + 2] = z + Math.cos(this.frame + z) * 0.1;
-        //         }
-
-        //         positionAttribute.needsUpdate = true;
-        //     }
-        // });
 
         object.traverse((child: any) => {
           if (child instanceof THREE.Mesh) {
@@ -224,10 +171,7 @@ export class ParticlesComponent implements AfterViewInit {
           }
         });
 
-        material.uniforms['time'].value += 0.1;
         this.scene.add(object);
-
-
       },
       (xhr: any) => {
         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -239,15 +183,11 @@ export class ParticlesComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.renderer.setSize(this.width, this.height * 0.85);
+    this.setRendererSize();
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
 
     window.addEventListener('resize', () => {
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-      this.renderer.setSize(width, height);
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
+      this.setRendererSize();
     });
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -266,6 +206,14 @@ export class ParticlesComponent implements AfterViewInit {
     this.controls.maxPolarAngle = Math.PI / 2;
 
     this.animate();
+  }
+
+  setRendererSize() {
+    const width = this.rendererContainer.nativeElement.clientWidth;
+    const height = this.rendererContainer.nativeElement.clientHeight;
+    this.renderer.setSize(width, height);
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
   }
 
   animate() {
